fd2cd2a8bd7f9a021944f59dcdbb6b011c1f9d15
Max-Pare <resilientcrab@proton.me>
2025-11-12 19:21:17 +0100

    Added flexible (and inefficient) wildcard system

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import random
+import sys
+import re
 
-responses_generic = ['Definitely not now', 'Definitely not today' , ' LATER! ', ' Later brah... ', " Can't do that right now brah ", ' Maybe later...', 'Maybe next __TIMEFRAME__']
+responses_generic = ['Definitely not (right now|now|today|this week)', 'LATER!', 'Later...', "Can't do that right now", 'Maybe later', 'Maybe (tomorrow|next week|next year|next month|later)']
 timeframes = ['week', 'month', 'year']
 
 def ask_cheech():
-    response = random.choice(responses_generic)
-    if '__TIMEFRAME__' in response:
-        return response.replace('__TIMEFRAME__', random.choice(timeframes))
+    def try_collapse(_response: str):
+        if (res := re.search('\((.*)\)', _response)) is None: return _response
+        found = res.group(1).split('|')
+        return _response.replace(res.group(), random.choice(found))
+    response = try_collapse(random.choice(responses_generic))
+    if random.random() < .2: response += ' brah'
     return response
 
 def main():
-    for _ in range (10):
+    for _ in range (32):
         print(ask_cheech())
 
 if __name__ == '__main__':
